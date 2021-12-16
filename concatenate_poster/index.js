5494057bf9ac585605fb8ae270b12f3bb06d69ea
JO3QMA <g.dark.matter@gmail.com>
2021-12-17 01:17:06 +0900

    トップページを作りた方・・・

--- concatenate_poster/index.js
+++ concatenate_poster/index.js
@@ -1,26 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.get('/about', function (req, res) {
-  res.send('About page');
-});
-
-app.get('/login', function(req, res) {
-  res.send('Login page');
-});
-
-app.get('/logout', function(req, res) {
-  res.send('Logout page');
-});
-
-app.get('/upload', function(req, res) {
-  res.send('Upload page');
-});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res, next) {
+  res.render('index', {});
 });
