9638b305ddf1f1cdd76bd272a0221a0ecafbfe67
JO3QMA <gumi@jo3qma.com>
2023-01-02 16:14:28 +0900

    左右分割まで実装。画像生成は未実装

---
+++ ruby/core/panel.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'rmagick'
+
+# 画像を複数枚結合する方
+class Panel
+  attr_writer :posters, :col, :row, :dummy, :separate_count
+
+  def initialize
+    @posters = []
+    @col = 4
+    @row = 4 * 2 # 2枚一組で生成するため、倍の長さが必要
+    @separate_count = 4
+  end
+
+  # ダミーで不足分を埋める
+  def fill_dummy
+    target_poster = @col * @row
+    if target_poster.size > @posters.size
+      fill_count = target_poster_size - @posters.size
+      fill_count.times do
+        @posters << @dummy
+      end
+    elsif @posters.size > target_poster_size
+      warn 'ERROR: ポスターの枚数が、目標値を超えています。'
+    end
+  end
+
+  # 左右で分割します。 @separate_count毎に分けられます。
+  def separate
+    @panels = { left: [], right: [] }
+    separate_posters = @posters.each_slice(@separate_count).to_a
+    separate_posters.each_with_index do |column, i|
+      if i.even?
+        @panels[:right] << column
+      else
+        @panels[:left] << column
+      end
+    end
+    @panels
+  end
+
+  def render; end
+end
